e98101c5d6b73b6d52d142dec9da68336f83fc3a
Tracy Collins <tracyecollins15@gmail.com>
2019-06-11 20:58:23 -0400

    pull

--- models/voyager.sql
+++ models/voyager.sql
@@ -8,7 +8,9 @@ CREATE TABLE voyager(
 	id int NOT NULL AUTO_INCREMENT,
 	astronaut_name varchar(50) NOT NULL,
     space_craft varchar(50) NOT NULL,
+	planet varchar(50) NOT NULL,
     trip_distance int(50) NOT NULL,
+	trip_time int(50) NOT NULL,
 	return_trip BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
